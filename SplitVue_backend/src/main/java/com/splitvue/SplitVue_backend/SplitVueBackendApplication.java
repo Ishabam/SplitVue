b3d00be04cbb39f1775b0e089ce035c1c43ae966
Ishabam <ishabam09@gmail.com>
2025-05-24 20:30:36 +0530

    Changes in repo structure

---
+++ SplitVue_backend/src/main/java/com/splitvue/SplitVue_backend/SplitVueBackendApplication.java
@@ -0,0 +1,13 @@
+package com.splitvue.SplitVue_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitVueBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SplitVueBackendApplication.class, args);
+	}
+
+}
---
+++ SplitVue_backend/src/test/java/com/splitvue/SplitVue_backend/SplitVueBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.splitvue.SplitVue_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SplitVueBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
